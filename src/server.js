cf01cd7575593a9e216030edd1e198d485d989b1
Aman <Sharmaamqn@gmail.com>
2026-02-12 10:19:16 +0530

    initialize the express server

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
